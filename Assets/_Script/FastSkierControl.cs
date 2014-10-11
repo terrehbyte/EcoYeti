2be251b2e0f3ceddd4dfdb8e0ebe9db70922dd77
kevincbryan <kevincbryan@gmail.com>
2014-10-11 16:26:41 -0700

    Added more skiers
    Fast Skiers
    Bob and Weave Skiers
    Added Trigger Zones to force Bob and Weave changes.
    Made Skiers colorful

---
+++ Assets/_Script/FastSkierControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FastSkierControl : MonoBehaviour {
+	
+	public int MovementMultiplier;
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Vector3 movement = new Vector3 (0.0f, 0.0f, -1.0f);
+		rigidbody.AddForce (movement*MovementMultiplier);
+	}
+}
