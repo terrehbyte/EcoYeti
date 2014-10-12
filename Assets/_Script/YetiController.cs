5e29e64f004424c8f2098ffbf7bf67b4a80aaed1
Terry Nguyen <terry@terrehbyte.com>
2014-10-11 17:12:02 -0700

    New YetiController

--- Assets/_Script/YetiController.cs
+++ Assets/_Script/YetiController.cs
@@ -1,19 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class YetiController : MonoBehaviour {
+public class YetiController : MonoBehaviour
+{
+    public float MoveSpeed = 5;
 
-	public GameObject player;
+    private float rayLength = 10;
+    
 
+    Vector3 AcceptInput()
+    {
+        Vector3 input = Vector3.zero;
 
-	
-	// Update is called once per frame
-	void LateUpdate () {
+        input.x = Input.GetAxis("Horizontal");
+        input.z = Input.GetAxis("Vertical");
 
-		transform.position = player.transform.position;
-	
-	}
+        return input;
+    }
 
+    void keepToGround()
+    {
+        Ray downRay = new Ray(transform.position, -Vector3.up);
+        RaycastHit[] hitInfo = Physics.RaycastAll(downRay, rayLength);
+        Vector3 target = Vector3.zero;
+        float distance = 0f;
 
+        for (int i = 0; i < hitInfo.Length; i++)
+        {
+            if (hitInfo[i].collider != collider)
+            {
+                target = hitInfo[i].point;
+                distance = hitInfo[i].distance;
+                break;
+            }
+        }
+
+        if (target != Vector3.zero && distance != 0f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, 0.1f);
+        }
+    }
+
+    #region Unity Events
+
+    void FixedUpdate()
+    {
+        Vector3 input = AcceptInput();
+        Vector3 newVel = Vector3.zero;
+
+        newVel.x = input.x * MoveSpeed;
+        newVel.y = rigidbody.velocity.y;
+        newVel.z = input.z * MoveSpeed;
+
+        rigidbody.velocity = newVel;
+        
+        keepToGround();
+
+        rigidbody.AddForce(Physics.gravity, ForceMode.Force);
+
+        // Rie Tanaka
+        // Kugimiya Rie
+        // Aoi Yuki
+        // Kana Hanazawa
+        // Eri Kitamuri
+        // Yu Asakawa
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+    }
+    #endregion
 }
-//Destroy (other.gameObject);
