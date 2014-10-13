14326c72ced29bc1face86f05fc53e64d5ebd3d2
kevincbryan <kevincbryan@gmail.com>
2014-10-12 17:56:00 -0700

    Pause On Main Game

---
+++ Assets/_Script/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	bool paused = false;
+	KeyCode pauseButton = KeyCode.Escape;
+	public GameObject MainMenu;
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+				if (Input.GetKeyDown(pauseButton))
+						paused = togglePause ();
+		}
+
+	bool togglePause()
+	{
+		if (Time.timeScale == 0f) {
+						Time.timeScale = 1f;
+						MainMenu.SetActive (false);
+						return(false);
+		} 
+		else 
+		{
+			Time.timeScale = 0f;
+			MainMenu.SetActive (true);
+			return(true);
+		}
+
+
+	}
+}
