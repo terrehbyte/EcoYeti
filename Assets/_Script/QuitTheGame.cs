fcfa2237dd7273c4178f8c619cc2e926cfe2f553
kevincbryan <kevincbryan@gmail.com>
2014-10-12 17:35:43 -0700

    FixedButtons

---
+++ Assets/_Script/QuitTheGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitTheGame : MonoBehaviour {
+
+	// Use this for initialization
+	public void QuitNow()
+	{
+		Application.Quit();
+	}
+}
